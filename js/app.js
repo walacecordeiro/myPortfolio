896d20c9f1490b87fb2677fd13e50a6b6e51bcde
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-02-22 21:08:47 -0300

    decided to change the layout

--- js/app.js
+++
@@ -1,8 +0,0 @@
-import { GithubUser } from "./GithubUser.js";
-
-const menuToggle = document.querySelector('.button-toggle')
-const nav = document.querySelector('nav')
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('show')
-})
