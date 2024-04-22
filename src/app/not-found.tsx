362e07a0c02adb23ea63bfb9631404456a5994a7
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-04-22 11:31:24 -0300

    alguns pequenos ajustes no loader e na 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,27 @@
-import Link from 'next/link'
- 
+import Link from "next/link";
+
 export default function NotFound() {
-  return (
-    <div>
-      <h2>Página não encontrada</h2>
-      <Link href="/">Retornar para o início</Link>
-    </div>
-  )
+ return (
+  <div
+   style={{
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 50,
+    height: "100vh",
+   }}
+  >
+   <h2>Página não encontrada</h2>
+   <Link
+    href="/"
+    style={{
+     color: "blue",
+     fontSize: 20
+    }}
+   >
+    Retornar para o início
+   </Link>
+  </div>
+ );
 }
