b33e070804fa026b99c0dfec8b68e000097eaec8
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-04-25 01:24:28 -0300

    adicionei skills no meu resumo

--- src/app/(content)/resumo/page.tsx
+++ src/app/(content)/resumo/page.tsx
@@ -4,16 +4,22 @@ import Footer from "@/app/components/Footer";
 import Image from "next/image";
 import perfil from "../../../../public/perfil.webp";
 import { FaLinkedin } from "react-icons/fa";
+import { FaSquareGithub } from "react-icons/fa6";
 import ImageModal from "@/app/components/ImageModal";
+import { resumeData } from "./resumeData";
 
 export default function Sobre() {
+ const justExperience = resumeData.filter((skill) => skill.justExperience);
+
+ const noExperienceSkill = resumeData.filter((skill) => !skill.justExperience);
+
  return (
   <main className={style["main"]}>
-   <Image src={perfil} width={150} height={150} alt="Minha foto de perfil" />
+   <Image priority src={perfil} width={150} height={150} alt="Minha foto de perfil" />
    <section className={style["cols2"]}>
     <div className={style["col1"]}>
      <h3>Walace Cordeiro dos Santos</h3>
-     <h6>Desenvolvedor Front-End &#124; Gestor de trafego</h6>
+     <h6>Desenvolvedor Front-End &#124; Gestor de tráfego</h6>
      <p>
       Minha rotina envolve a utilização de tecnologias fundamentais como{" "}
       <strong>HTML, CSS e JavaScript</strong>. Tenho uma forte inclinação para frameworks modernos,
@@ -27,8 +33,15 @@ export default function Sobre() {
      <p>+55 21 98877-8644</p>
      <p>Rio de Janeiro, Campo Grande</p>
      <div className={style["icons"]}>
-      <a href="https://www.linkedin.com/in/walace-cordeiro-dos-santos/" target="_blanck">
-       <FaLinkedin className="icons" />
+      <a
+       title="LinkedIn"
+       href="https://www.linkedin.com/in/walace-cordeiro-dos-santos/"
+       target="_blanck"
+      >
+       <FaLinkedin />
+      </a>
+      <a title="GitHub" href="https://github.com/walacecordeiro" target="_blanck">
+       <FaSquareGithub />
       </a>
      </div>
     </div>
@@ -45,7 +58,37 @@ export default function Sobre() {
     </div>
 
     <div className={style["col2"]}>
-     <h4>Skills</h4>
+     {noExperienceSkill && (
+      <>
+       <h4>Skills</h4>
+       <div className={style["icons"]}>
+        <div className={style["wrappingSkills"]}>
+         {noExperienceSkill.map((data, i) => (
+          <div className={style["skills"]} key={i}>
+           <img src={data.icon} title={data.text} />
+           <p>{data.text}</p>
+          </div>
+         ))}
+        </div>
+       </div>
+      </>
+     )}
+
+     {justExperience && (
+      <>
+       <h4>Conhecimento</h4>
+       <div className={style["icons"]}>
+        <div className={style["wrappingSkills"]}>
+         {justExperience.map((skill, i) => (
+          <div className={style["skills"]} key={i}>
+           <img src={skill.icon} title={skill.text} />
+           <p>{skill.text}</p>
+          </div>
+         ))}
+        </div>
+       </div>
+      </>
+     )}
     </div>
    </section>
    <Footer />
---
+++ src/app/(content)/resumo/resumeData.js
@@ -0,0 +1,14 @@
+export const resumeData = [
+ { text: "HTML5", icon: "/icons/iconHTML5.svg", justExperience: false },
+ { text: "CSS3", icon: "/icons/iconCSS3.svg", justExperience: false },
+ { text: "TailwindCSS", icon: "/icons/iconTailwindCSS.svg", justExperience: false },
+ { text: "SASS/SCSS", icon: "/icons/iconSASS.svg", justExperience: false },
+ { text: "JavaScript", icon: "/icons/iconJavaScript.svg", justExperience: false },
+ { text: "TypeScript", icon: "/icons/iconTypeScript.svg", justExperience: false },
+ { text: "React/Native", icon: "/icons/iconReactJs.svg", justExperience: false },
+ { text: "NextJs", icon: "/icons/iconNextJs.svg", justExperience: false },
+ { text: "GitHub", icon: "/icons/iconGitHub.svg", justExperience: false },
+ { text: "Figma", icon: "/icons/iconFigma.svg", justExperience: true },
+ { text: "WordPress", icon: "/icons/iconWordPress.svg", justExperience: true },
+ { text: "Google ADS", icon: "/icons/iconGoogleADS.svg", justExperience: true },
+];
